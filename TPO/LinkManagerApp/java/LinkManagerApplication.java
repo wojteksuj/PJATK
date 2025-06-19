cae46f7b15100c143bd359f35803f8099eb48af5
Wojciech Sujczyński <wojciechsuj@gmail.com>
2025-06-19 14:05:01 +0200

    Create LinkManagerApplication.java

---
+++ TPO/LinkManagerApp/java/LinkManagerApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkManagerApplication.class, args);
+    }
+}
