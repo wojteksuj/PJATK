47d75a83e7f6564aef3b028dd6d907f5dceb8ee1
Wojciech Sujczyński <wojciechsuj@gmail.com>
2025-06-19 13:46:13 +0200

    Add files via upload

---
+++ NAI/S31087Tpo10Application.java
@@ -0,0 +1,15 @@
+package org.example.s31087tpo10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S31087Tpo10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S31087Tpo10Application.class, args);
+    }
+
+
+
+}
